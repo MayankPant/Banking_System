5c24b7ded32826064bf0381e249596450fa6d074
Mayank Pant <mpant4512@gmail.com>
2022-07-19 01:55:56 +0530

    Implemented the removeAccount() method for banking management system.
    After adding the transaction relating to deletion of account we remove
    the account. Further improvements can be done later.

--- src/BankingSystem/BankingManagementSystem.java
+++ src/BankingSystem/BankingManagementSystem.java
@@ -1,5 +1,6 @@
 package BankingSystem;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 
 import java.util.Date;
@@ -63,6 +64,68 @@ public class BankingManagementSystem {
         {
             System.out.println(e.getMessage());
             CreateSessionFactory.sessionFactory.close();
+        }
+
+    }
+
+    public static void removeAccount(String name, long accountNo)
+    {
+        System.out.println("Are you sure you want to remove your account? Enter Y for yes and N for no.");
+        char option = sc.next().charAt(0);
+        boolean menuControl = true;
+
+        while (menuControl)
+        {
+            switch (option)
+            {
+                case 'Y':
+                    menuControl = false;
+                    break;
+                case 'N':
+                    System.out.println("Account removal request aborted.");
+                    return;
+                default:
+                    System.out.println("Please enter a valid option or type \"QUIT\" to exit.");
+                    String quitOption = sc.next();
+                    if(quitOption.equals("QUIT"))
+                    {
+                        System.out.println("Thanks for working with us.");
+                        menuControl =  false;
+                        break;
+                    }
+                    else
+                    {
+                        option = quitOption.charAt(0);
+                        break;
+                    }
+            }
+
+        }
+        try {
+            Session session = CreateSessionFactory.sessionFactory.openSession();
+
+            Account account = session.get(Account.class, accountNo);
+            // adding the log of deletion.
+            if(account.getName().equals(name))
+            {
+                Transaction transaction = new Transaction(Constants.REMOVE, new Date(), account);
+                account.getTransactionHistory().add(transaction);
+
+                System.out.println(transaction.getTransactionDescription());
+
+                session.beginTransaction();
+                session.remove(account);
+                session.getTransaction().commit();
+            }
+
+            else
+            {
+                System.out.println("Your name is wrong.");
+            }
+
+            session.close();
+        } catch (HibernateException e) {
+            e.printStackTrace();
         }
 
     }
--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,8 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
-        BankingManagementSystem.createAccount("Amit","1234","apant","Shekhar");
-        BankingManagementSystem.createAccount("Mayank","345","mpant","shashank");
+//        BankingManagementSystem.createAccount("Amit","1234","apant","Shekhar");
+//        BankingManagementSystem.createAccount("Mayank","345","mpant","shashank");
+        BankingManagementSystem.removeAccount("Amit",2175171210146877L);
     }
 }
